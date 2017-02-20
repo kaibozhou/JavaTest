948b4de8561e8cbf977915b3f4f560095f8efcd6
kaibo.zhou <zkb555@gmail.com>
2017-02-20 14:25:46 +0800

    add JsonFormatUtil

---
+++ JsonFormatUtil.java
@@ -0,0 +1,57 @@
+package com.alibaba.blink.scala.tool.util;
+
+/**
+ * Format json string
+ */
+public class JsonFormatUtil {
+
+	public static String formatJson(String jsonStr) {
+		if (null == jsonStr || "".equals(jsonStr)) return "";
+		StringBuilder sb = new StringBuilder();
+		char last = '\0';
+		char current = '\0';
+		int indent = 0;
+		for (int i = 0; i < jsonStr.length(); i++) {
+			last = current;
+			current = jsonStr.charAt(i);
+			switch (current) {
+				case '{':
+				case '[':
+					sb.append(current);
+					sb.append('\n');
+					indent++;
+					addIndentBlank(sb, indent);
+					break;
+				case '}':
+				case ']':
+					sb.append('\n');
+					indent--;
+					addIndentBlank(sb, indent);
+					sb.append(current);
+					break;
+				case ',':
+					sb.append(current);
+					if (last != '\\') {
+						sb.append('\n');
+						addIndentBlank(sb, indent);
+					}
+					break;
+				default:
+					sb.append(current);
+			}
+		}
+
+		return sb.toString();
+	}
+
+	/**
+	 * Append indent to input string
+	 * @param sb Input String
+	 * @param indent Indent number
+	 */
+	private static void addIndentBlank(StringBuilder sb, int indent) {
+		for (int i = 0; i < indent; i++) {
+			sb.append('\t');
+		}
+	}
+}
